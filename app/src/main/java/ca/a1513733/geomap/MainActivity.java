7c0e431ad544c97fa27abf8057198dc002da3c4c
Rhai Hinds <rhai.hinds@dawsoncollege.qc.ca>
2017-09-25 11:20:12 -0400

    New geo map project

---
+++ app/src/main/java/ca/a1513733/geomap/MainActivity.java
@@ -0,0 +1,17 @@
+package ca.a1513733.geomap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ShowMap() {
+
+    }
+}
